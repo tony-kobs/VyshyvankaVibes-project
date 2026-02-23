7b129732be2f93b03d4bf380dc17181bb51daa2f
akrep17m-png <akr.ep17m@gmail.com>
2026-02-23 17:49:56 +0200

    feat: add gh-pages dependency for GitHub Pages deployment
    Added the gh-pages package to the project's devDependencies to enable automated deployment to GitHub Pages. This will allow the project to be easily published and hosted on GitHub Pages for public access.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/VyshyvankaVibes-project/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
